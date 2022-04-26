a95a4b7486243125dfd81e6039833b6f4a49985a
Steven Hsu <leohsu5833@gmail.com>
2022-04-26 10:34:39 +0800

    add go to second activity scenario

---
+++ app/src/main/java/com/example/demo3/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo3;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
